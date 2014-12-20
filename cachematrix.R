5e1ac087aebf3a54a7443bb09699b7d68fa09204
bokyungs <bokyungs@gmail.com>
2014-12-19 20:31:13 -0600

    check-in

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,41 @@
 ## create a special matrix object that can cache its inverse
+## it uses a superassignment operator <<- to assign a value to a variable that's outside the environment the function
+# is defined.
 
 
 ## return a list of 4 operations: set, get, setInverse, and getInverse
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    inv <- NULL
     
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        inv <<- NULL
     }
     
-    get <- function() x
-    setInverse <- function(newMatrix) m <<- newMatrix
-    getInverse<- function() m
+    get <- function() x  # return the original matrix
+    setInverse <- function(newMatrix) inv <<- newMatrix  #sets inv to the new inverse matrix
+    getInverse<- function() inv # return the inverse matrix
     
-    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) # each element in the list contains a function.
 
 }
 
 
-## if m is not null, return the cached data.  If it's null, compute
-## the inverse and assign it to m.
+## if inv is not null, return the cached data.  If it's null, compute
+## the inverse and assign it to inv.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    m <- x$getInverse()
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
     
-    if (!is.null(m)) {
+    if (!is.null(inv)) { #inverse matrix exists
         message("getting cached data")
-        return(m)
+        return(inv)
     }
     
     data <- x$get()
-    m <- solve(data, ...)
-    x$setInverse(m)
+    inv <- solve(data, ...) # compute the inverse matrix and assign it to inv
+    x$setInverse(inv)
     
-    m
+    inv
 }
